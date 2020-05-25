b4fed24bfc9bd641ac946a6abf0dcff4e1b027f3
SL33PiNg <nook3920@gmail.com>
2020-05-25 17:49:06 +0700

    add mapbox-gl

---
+++ src/renderer/components/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="map" />
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+export default {
+  data () {
+    return {
+      latlng: '',
+      accessToken: 'pk.eyJ1Ijoibm9vazM5MjAiLCJhIjoiY2s5OTlycGRkMDEzbjNlbWt0eWJpYWo3OCJ9.STeeCOccG7hVQ8aiktTRFw',
+      map: {}
+    }
+  },
+  mounted () {
+    this.createMap()
+  },
+  methods: {
+    createMap () {
+      mapboxgl.accessToken = this.accessToken
+      this.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [100.5923, 13.9255],
+        zoom: 10
+      })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+#map{
+   width:100%;
+   height:100vh;
+}
+</style>
